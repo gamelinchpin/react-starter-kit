df646053eb80840162708e7807d5f001673c8348
Simeon Pashley <simeon@ringalpha.com>
2015-10-12 23:10:50 +0100

    docker link with AFAPI

--- docker/node/init.sh
+++ docker/node/init.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 apt-get upgrade -y
+
+# install nodejs + npm.  (source: https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server)
+apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_4.x | bash -
+# end ppa nodejs
 apt-get update -y
-
-
-# # install nodejs + npm.  (source: https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server)
-# apt-get install -y curl
-# curl -sL https://deb.nodesource.com/setup | bash -
-# apt-get update -y
-# # end ppa nodejs
-
 apt-get install -y git nodejs build-essential libssl-dev
 
 # Install packages
